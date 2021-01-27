813cb47c79a153cad342ad709d46bcd4372624b5
GitHub Action <action@github.com>
2021-01-27 09:49:29 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/xiangjing27/autocodegenerate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.xiangjing27.autocodegenerate.services
+
+import com.github.xiangjing27.autocodegenerate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
